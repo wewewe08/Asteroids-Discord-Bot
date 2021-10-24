95b7f5482a3e2d41027144ff23a9886a07d4c5e6
yvonne <31106392+wewewe08@users.noreply.github.com>
2021-10-24 13:43:30 -0400

    Update Movement.py

--- cogs/Movement.py
+++ cogs/Movement.py
@@ -32,40 +32,5 @@ class Movement(commands.Cog):
         gameboard[0][6] = arr
         return gameboard
 
-    async def moveAsteroid(self, gameboard, arr):
-        rows = len(gameboard)
-        cols = len(gameboard[0])
-        grid_length = rows*cols
-        for row in range(0, grid_length):
-            for col in range(0, row):
-                if gameboard[0][col] == arr:
-                    if col != 6:
-                        gameboard[0][col] = gameboard[0][col+1]
-                        gameboard[0][col+1] = arr
-                    else:
-                        print("something went wrong with the asteroid")
-                        pass
-                break
-        return gameboard  
-
-    async def shootAsteroid(self, gameboard, arrRocket, arrAsteroid):
-        for index in range(len(arrRocket)):
-            if arrRocket[index] == ":rocket:":
-                rocketIndex = index
-                if arrAsteroid[rocketIndex] == ":rock:":
-                    arrAsteroid[rocketIndex] = ":boom:"
-                else:
-                    print("something went wrong with shooting")
-                    pass
-                break
-        return gameboard
-
-    async def resetBoard(self, gameboard, arrAsteroid):
-        for index in range(len(arrAsteroid)):
-            if arrAsteroid[index] == ":boom:":
-                arrAsteroid[index] = ":black_large_square:"
-                break
-        return gameboard 
-
 def setup(client):
     client.add_cog(Movement(client))
