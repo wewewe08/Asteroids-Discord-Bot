46cc5a63d53130b8b73788c74a24d4a698b6a408
yvonne <31106392+wewewe08@users.noreply.github.com>
2021-10-24 22:08:47 -0400

    added missle and point count

--- cogs/Play.py
+++ cogs/Play.py
@@ -1,7 +1,6 @@
 import discord
 import asyncio
 import datetime
-
 from discord.ext import commands
 
 class Play(commands.Cog):
@@ -26,32 +25,27 @@ class Play(commands.Cog):
         icon = ":black_large_square:"
         #grid variables
         grid_size = 7
-        #losing condition
-        game_over = False
+        #game variables
+        isRock = False
+        points = 0
+        missles = 10
 
         if gameGrid is not None and user_input is not None and makeEmbed is not None and asteroid is not None:
             #setting the array variables for the grids
-            gameboard, arrAsteroid, arrRocket = await gameGrid.makeGrid(grid_size, icon)
-
+            gameboard, arrAsteroid, arrRocket, arrPackage = await gameGrid.makeGrid(grid_size, icon)
             #send the gameboard to discord as an EMBED
             origGrid = await gameGrid.gridToString(stringVal= "", gameboard = gameboard)
-            embed = await makeEmbed.makeEmbed(ctx, origGrid, image_url)
+            embed = await makeEmbed.makeEmbed(ctx, origGrid, image_url, points, missles)
             message = await ctx.send(embed = embed)
 
             emojis = ["⬅️", "➡️", "💥"] #player input
             for e in emojis:
                 await message.add_reaction(emoji = e)      
-
             #check if the user reacted to the message
             def checkReaction(reaction, user):
                 return user == ctx.message.author and str(reaction.emoji) in emojis
 
-            while not game_over:  
-                #move asteroids (ARCHIVED)
-                #generator = await asteroid.moveAsteroid(gameboard, arrAsteroid)
-                #for item in generator:
-                #    await message.edit(embed = resetEmbed(item))
-                #    await asyncio.sleep(1)
+            while missles != 0 and points != 10:  
                 asteroidShot = False
                 try:
                     reaction, user = await self.client.wait_for("reaction_add", timeout= 20.0, check = checkReaction)
@@ -60,19 +54,29 @@ class Play(commands.Cog):
                     elif str(reaction.emoji) == '➡️':
                         move = await user_input.moveRight(gameboard, arrRocket)
                     elif str(reaction.emoji) == '💥':
-                        move = await asteroid.shootAsteroid(gameboard, arrRocket, arrAsteroid)
+                        move,isRock,missles = await asteroid.shootAsteroid(gameboard, arrRocket, arrAsteroid, arrPackage, missles, isRock)
                         asteroidShot = True
+                        if isRock:
+                            points += 1
+                            missles -= 1
+                            isRock = False
+                        else:
+                            pass
                 except asyncio.TimeoutError:
                     await ctx.send(f"> **{ctx.author.name}, you didn't react to the message so the game ended.**") #afk player
                     break
                 else:
                     player = ctx.message.author
-                    reset_embed = await makeEmbed.resetEmbed(ctx, move, image_url)
+                    reset_embed = await makeEmbed.resetEmbed(ctx, move, image_url, points, missles)
                     await message.edit(embed = reset_embed) #reset grid
-                    move, arrAsteroid = await asteroid.resetBoard(gameboard, arrAsteroid, arrRocket, grid_size, icon, asteroidShot)
+                    move, arrAsteroid, arrPackage = await asteroid.resetBoard(gameboard, arrAsteroid, arrRocket, arrPackage, grid_size, icon, asteroidShot)
                     for e in emojis:
                         await message.remove_reaction(e, player)   
                         asteroidShot = False    
+            if missles == 0 and points != 10:
+                await ctx.send(f"> ** :octagonal_sign:  you are out of missles! better luck next time, {ctx.author.name}! **")
+            elif points == 10:
+                await ctx.send(f"> ** :medal: congratulations, {ctx.author.name}, you won!**")
 
     async def cog_command_error(self, ctx, error):
         if isinstance(error, commands.MaxConcurrencyReached):
