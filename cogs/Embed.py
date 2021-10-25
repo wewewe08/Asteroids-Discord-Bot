052389db4ebc73b5a8ae22dc8665e941090b8c89
yvonne <31106392+wewewe08@users.noreply.github.com>
2021-10-24 22:24:13 -0400

    typo

--- cogs/Embed.py
+++ cogs/Embed.py
@@ -15,7 +15,7 @@ class Embed(commands.Cog):
             title = "ASTEROIDS :rocket: ",
         )
         embed.timestamp = datetime.datetime.utcnow()
-        embed.add_field(name = ":game_die: HOW TO PLAY :game_die: ", value = "1. react to the messages to move/shoot (⬅️, ➡️, 💥) \n2. move the rocket after you shoot an asteroid to respawn them :rock:\n3. shoot the care packages to replenish ammo! ( 📦 )\n4. care packages have a random chance of spawning after shooting a rock.\n5. reach 10 points to win! :medal: ", inline = False)
+        embed.add_field(name = ":game_die: HOW TO PLAY :game_die: ", value = "1. react to the messages to move/shoot (⬅️, ➡️, 💥) \n2. move the rocket after you shoot an asteroid to respawn them :rock:\n3. shoot the care packages to replenish ammo! ( 📦 )\n4. care packages have a random chance of spawning after shooting a rock.\n5. reach 5 points to win! :medal: ", inline = False)
         embed.add_field(name = "WARNING⚠️", value = "DO NOT react too fast - make sure that the last reaction has reset before reacting again.", inline = False)
         embed.add_field(name = f"🥉POINTS: {points}    📦MISSLES: {missles}", value = grid, inline = True)
         embed.set_footer(text= f"{ctx.author.name}'s game",icon_url=url)
@@ -29,11 +29,11 @@ class Embed(commands.Cog):
                 title = "ASTEROIDS :rocket:  ",
             )
             new_embed.timestamp = datetime.datetime.utcnow()
-            new_embed.add_field(name = ":game_die: HOW TO PLAY :game_die: ", value = "1. react to the messages to move/shoot (⬅️, ➡️, 💥) \n2. move the rocket after you shoot an asteroid to respawn them :rock:\n3. shoot the care packages to replenish ammo! ( 📦 )\n4. care packages have a random chance of spawning after shooting a rock.\n5. reach 10 points to win! :medal: ", inline = False)
+            new_embed.add_field(name = ":game_die: HOW TO PLAY :game_die: ", value = "1. react to the messages to move/shoot (⬅️, ➡️, 💥) \n2. move the rocket after you shoot an asteroid to respawn them :rock:\n3. shoot the care packages to replenish ammo! ( 📦 )\n4. care packages have a random chance of spawning after shooting a rock.\n5. reach 5 points to win! :medal: ", inline = False)
             new_embed.add_field(name = "WARNING⚠️", value = "DO NOT react too fast - make sure that the last reaction has reset before reacting again.", inline = False)
-            if points >= 10:
+            if points >= 5:
                 new_embed.add_field(name = f"🥇POINTS: {points}    📦MISSLES: {missles}", value = newGrid, inline = True) #
-            elif points >=  5 and points < 10:
+            elif points >=  3 and points < 5:
                 new_embed.add_field(name = f"🥈POINTS: {points}    📦MISSLES: {missles}", value = newGrid, inline = True)
             else:
                 new_embed.add_field(name = f"🥉POINTS: {points}    📦MISSLES: {missles} ", value = newGrid, inline = True)
